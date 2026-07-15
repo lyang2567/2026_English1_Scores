a579935b57c599d6b06ef3745d99553e7c18889c
lyang2567 <lyang5267@gmail.com>
2026-07-15 13:25:04 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# English1 Scores
+
+A simple demo project.
+
+Open index.html in a browser or deploy with GitHub Pages.
---
+++ app.js
@@ -0,0 +1,4 @@
+function search(){
+document.getElementById('result').innerHTML=
+'<h3>Demo Result</h3><p>Name: Demo Student</p><p>Course: English I</p><p>Score: 90</p>';
+}
